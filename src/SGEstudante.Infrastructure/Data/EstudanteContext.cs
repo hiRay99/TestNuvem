31f3cbe6f2a1e70f2718c68208885e8e3777e6f6
Rayane Ribeiro <rayaneribeirofe@gmail.com>
2018-05-16 21:43:11 -0400

    2

---
+++ src/SGEstudante.Infrastructure/Data/EstudanteContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGEstudante.Infrastructure.Data
+{
+    public class EstudanteContext : DbContext
+    {
+        public EstudanteContext(DbContextOptions<EstudanteContext> options) : base(options)
+        {
+                
+        }
+    }
+}
