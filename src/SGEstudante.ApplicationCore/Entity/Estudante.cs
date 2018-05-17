c17fb4b7befd662db494b52d545b937b436e2c33
Rayane Ribeiro <rayaneribeirofe@gmail.com>
2018-05-16 21:18:24 -0400

    1

---
+++ src/SGEstudante.ApplicationCore/Entity/Estudante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGEstudante.ApplicationCore.Entity
+{
+   public class Estudante
+    {
+        public Estudante()
+        {
+
+        }
+
+        public int EstudanteId { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Incricao { get; set; }
+       
+    }
+}
