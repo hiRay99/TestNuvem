45b1d2d352c6ad39ff1cf6aa7b15d1541021c8a3
Rayane Ribeiro <rayaneribeiro99@hotmail.com>
2018-05-18 13:54:44 -0400

    Atulizando o banco

--- src/SGEstudante.Infrastructure/Data/EstudanteContext.cs
+++ src/SGEstudante.Infrastructure/Data/EstudanteContext.cs
@@ -21,9 +21,34 @@ namespace SGEstudante.Infrastructure.Data
             modelBuilder.Entity<Estudante>().ToTable("Estudante");
             modelBuilder.Entity<Contato>().ToTable("Contato");
 
+            #region Configuração de Estudante
+
             modelBuilder.Entity<Estudante>().Property(e => e.CPF)
             .HasColumnType("varchar(11)")
             .IsRequired();
+
+            modelBuilder.Entity<Estudante>().Property(e => e.Nome)
+            .HasColumnType("varchar(200)")
+            .IsRequired();
+
+            #endregion
+
+            #region Configuração de Contato
+
+            modelBuilder.Entity<Contato>().Property(e => e.Nome)
+            .HasColumnType("varchar(200)")
+            .IsRequired();
+
+            modelBuilder.Entity<Contato>().Property(e => e.Email)
+            .HasColumnType("varchar(100)")
+            .IsRequired();
+
+            modelBuilder.Entity<Contato>().Property(e => e.Telefone)
+            .HasColumnType("varchar(15)");
+
+            #endregion
         }
+    }
+    }
     }
 }
